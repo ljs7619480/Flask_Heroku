720b86887c0d39644103c45fdfe91d264b6e76d4
Jason Li <fkl7619480@gmail.com>
2021-10-24 10:56:55 +0800

    district

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Created on Sat Aug 18 01:00:17 2018
 
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import json
+import json, csv
 
 # class EntryEncoder(json.JSONEncoder):
 #     def default(self, obj):
@@ -20,19 +20,30 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://104-2:nycuisgood@hackathon.ugeyw.mongodb.net/job"
 mongo = PyMongo(app)
 
+dist_map = {}
+with open('104/category/district.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file,)
+    title  = csv_reader.fieldnames
+    for row in csv_reader.reader:
+        dist_map[int(row[0])] = row[1]
+
+
 @app.route("/")
 def hello():
     return render_template('mont.html')
 
+
 #https://pymongo.readthedocs.io/en/stable/api/pymongo/collection.html#pymongo.collection.Collection.find
-@app.route("/job/<custno>/<next_job>")
-def home_page(custno, next_job):
-    data_cursor = mongo.db.processed_v5.find({"custno":custno, "next_job": next_job})
+@app.route("/job/<custno>/<jobno>")
+def home_page(custno, jobno):
+    data_cursor = mongo.db.processed_v5.find({"custno":custno, "jobno": jobno})
     data = list(data_cursor[:3])
     for d in data:
         d.pop("_id")
 
-    return json.dumps(data)
+    return render_template('index104.html', 
+                           next_job=data[0]['next_job'],
+                           district=dist_map[data[0]['addr_no']])
 
 
 if __name__ == '__main__':
